2485517235e34e1d6954eab7158f0d9a5f9e61e6
MichaelHerrington2020 <michael.herrington@utexas.edu>
2019-04-02 20:34:13 -0500

    Finished code still testing

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -1,13 +1,18 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.join.TupleWritable;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -22,12 +27,13 @@ public class TextAnalyzer extends Configured implements Tool {
     // Replace "?" with your own output key / value types
     // The four template data types are:
     //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
-    public static class TextMapper extends Mapper<LongWritable, Text, TupleWritable, IntWritable> {
+    public static class TextMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
-        private final static Text[] wordArray = new Text[2];
+        private final static Text wordPair = new Text();
 
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
+
             // Implementation of you mapper function
             String line = value.toString();
             //line = line.replaceAll("[^a-zA-Z\\d\\s:]", " ");
@@ -38,14 +44,12 @@ public class TextAnalyzer extends Configured implements Tool {
             System.out.println(Arrays.asList(tokens));
             for (int i = 0; i < tokens.length; i++) {
                 for (int j = i+1; j < tokens.length; j++) {
-                    wordArray[0].set(tokens[i]);
-                    wordArray[1].set(tokens[j]);
-                    TupleWritable wordTuple = new TupleWritable(wordArray);
-                    context.write(wordTuple, one);
-//                    wordArray[0].set(tokens[j]);
-//                    wordArray[1].set(tokens[i]);
-//                    wordTuple = new TupleWritable(wordArray);
-//                    context.write(wordTuple, one);
+                    if (tokens[0].equals("") || tokens[1].equals(""))
+                        continue;
+                    wordPair.set(tokens[0] + " " + tokens[1]);
+                    context.write(wordPair, one);
+                    wordPair.set(tokens[1] + " " + tokens[0]);
+                    context.write(wordPair, one);
                 }
             }
         }
@@ -53,9 +57,9 @@ public class TextAnalyzer extends Configured implements Tool {
 
     // Replace "?" with your own key / value types
     // NOTE: combiner's output key / value types have to be the same as those of mapper
-    public static class TextCombiner extends Reducer<TupleWritable, IntWritable, TupleWritable, IntWritable> {
+    public static class TextCombiner extends Reducer<Text,IntWritable,Text,IntWritable> {
         private final static IntWritable result = new IntWritable();
-        public void reduce(TupleWritable key, Iterable<IntWritable> values, Context context)
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             // Implementation of you combiner function
             int sum = 0;
@@ -69,35 +73,25 @@ public class TextAnalyzer extends Configured implements Tool {
 
     // Replace "?" with your own input key / value types, i.e., the output
     // key / value types of your mapper function
-    public static class TextReducer extends Reducer<TupleWritable, IntWritable, Text, Text> {
+    public static class TextReducer extends Reducer<Text, IntWritable, Text, Text> {
         private final static Text emptyText = new Text("");
 
-        public void reduce(TupleWritable key, Iterable<IntWritable> values, Context context)
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             // Implementation of you reducer function
-            Text tupleString = new Text();
             Text value = new Text();
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
-            value.set(String.valueOf(sum));
-            StringBuilder builder = new StringBuilder();
-            for (Writable writable : key) {
-                builder.append(writable.toString()).append(" ");
-            }
-            tupleString.set(builder.toString());
-            context.write(tupleString, value);
 
             // Write out the results; you may change the following example
             // code to fit with your reducer function.
-            //   Write out each edge and its weight
-//            for(String neighbor: map.keySet()){
-//                String weight = map.get(neighbor).toString();
-//                value.set(" " + neighbor + " " + weight);
-//                context.write(key, value);
-//            }
-            //   Empty line for ending the current context key
+            // Write out each edge and its weight
+            value.set(String.valueOf(sum));
+            context.write(key, value);
+
+            // Empty line for ending the current context key
             context.write(emptyText, emptyText);
         }
     }
@@ -121,7 +115,7 @@ public class TextAnalyzer extends Configured implements Tool {
         job.setOutputValueClass(Text.class);
         //   If your mapper and combiner's  output types are different from Text.class,
         //   then uncomment the following lines to specify the data types.
-        job.setMapOutputKeyClass(TupleWritable.class);
+        job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
         // Input
@@ -143,42 +137,4 @@ public class TextAnalyzer extends Configured implements Tool {
     }
 
     // You may define sub-classes here.
-    public static class WordPair implements Writable{
-        String s1;
-        String s2;
-        public WordPair(String s1, String s2) {
-            this.s1 = s1;
-            this.s2 = s2;
-        }
-
-        @Override
-        public String toString() {
-            return s1 + " " + s2;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            // usual equals checks
-            WordPair other = (WordPair) o;
-            if (this.s1.equals(other.s1) && this.s2.equals(other.s2))
-                return true;
-            else if (this.s1.equals(other.s2) && this.s2.equals(other.s1))
-                return true;
-            return false;
-        }
-
-        @Override
-        public void write(DataOutput dataOutput) throws IOException {
-            dataOutput.writeChars(toString());
-        }
-
-        @Override
-        public void readFields(DataInput dataInput) throws IOException {
-            String[] tokens = dataInput.readLine().split(" ");
-            s1 = tokens[0];
-            s2 = tokens[1];
-        }
-    }
 }
-
-
